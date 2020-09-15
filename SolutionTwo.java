40c2dc1c4f3787f588cfd1e54aa754658fca7656
RANU SHARMA <40596697+ranu160298@users.noreply.github.com>
2020-09-16 01:47:42 +0530

    Add files via upload

---
+++ SolutionTwo.java
@@ -0,0 +1,11 @@
+/*Print a message using command line argument*/
+package com.day.one;
+
+public class SolutionTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Message: "+args [0]);
+	}
+
+}
