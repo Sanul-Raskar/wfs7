eb7e328bbb76d531f9642ee7fa5fbe8a77315cac
Sanul Raskar <sanulraskar@gmail.com>
2020-09-16 19:35:28 +0530

    Day 1 assignment solutions

---
+++ Sanul-Raskar/Day1/src/com/example/pack/Welcome.java
@@ -0,0 +1,20 @@
+package com.example.pack;
+
+/*
+ * Lab Exercise 1
+ * This program prints the statement to the console
+ * 
+ * Created by Sanul Raskar
+ * 
+ * */
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Print the line to the console
+		System.out.println("Welcome to the world of Java.");
+	}
+
+}
