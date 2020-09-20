32c752714179983cf6a3aa2991a43b358384b068
rajdoubleO7 <71395563+rajdoubleO7@users.noreply.github.com>
2020-09-20 23:09:05 +0530

    Day - 4 Assignment

---
+++ Raj/Day 4/LE55/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+class ProductNotFoundException extends Exception{
+    public ProductNotFoundException(String s) 
+    { 
+        // Call constructor of parent Exception 
+        super(s); 
+    } 
+}
---
+++ Raj/Day 4/LE58/EmployeeWongDataException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsa;
+
+/**
+ *
+ * @author surya
+ */
+public class EmployeeWongDataException extends Exception{
+    public EmployeeWongDataException(String s) 
+    { 
+        // Call constructor of parent Exception 
+        super(s); 
+    } 
+}
